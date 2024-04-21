e85cdbd413e614f57c82dd5dbd53f47ae05f3fdd
alice <aliceisjustplaying@gmail.com>
2024-04-21 21:53:42 +0100

    nusan start

--- src/ext/fft_orig.c
+++ src/ext/fft_orig.c
@@ -191,55 +191,17 @@ void FFT_Close()
 }
 
 //////////////////////////////////////////////////////////////////////////
-float fFFTSmoothingFactor = 0.9f; // higher value, smoother FFT
-static float fftDataSmoothed[FFT_SIZE] = {0}; // Initialize the array with zeros
-static const float fPeakMinValue = 0.01f;
-static const float fPeakSmoothing = 0.995f;
-static float fPeakSmoothValue = 0.0f;
-
-double tic_api_fft(tic_mem* memory, s32 freq/*, bool bPeakNormalization, bool bSmoothing*/)
+double tic_api_fft(tic_mem* memory, s32 freq)
 {
   u32 interval = FFT_SIZE / 256 / 2; // the 2 is to discard super high frequencies, they suck
   freq = freq * interval;
   freq = fmin(freq, FFT_SIZE);
   freq = fmax(freq, 0);
-  
-  bool bPeakNormalization = true;
-  bool bSmoothing = true;
-
-  if (bPeakNormalization) {
-    float peakValue = fPeakMinValue;
-    for (int i = freq; i < freq + interval; ++i) {
-      float val = 2.0f * sqrtf(fftBuf[i].r * fftBuf[i].r + fftBuf[i].i * fftBuf[i].i);
-      if (val > peakValue) {
-        peakValue = val;
-      }
-    }
-    if (peakValue > fPeakSmoothValue) {
-      fPeakSmoothValue = peakValue;
-    }
-    if (peakValue < fPeakSmoothValue) {
-      fPeakSmoothValue = fPeakSmoothValue * fPeakSmoothing + peakValue * (1 - fPeakSmoothing);
-    }
-    if (fPeakSmoothValue > 0.0f) {
-      fAmplification = 1.0f / fPeakSmoothValue;
-    } else {
-      fAmplification = 1.0f;
-    }
-  }
 
   static const float scaling = 1.0f / (float)FFT_SIZE;
   float res = 0;
   for (int i = freq; i < freq + interval; ++i) {
-    float val = 2.0f * sqrtf(fftBuf[i].r * fftBuf[i].r + fftBuf[i].i * fftBuf[i].i) * scaling * fAmplification;
-    if (bSmoothing) {
-      if (i < FFT_SIZE) {
-        fftDataSmoothed[i] = fftDataSmoothed[i] * fFFTSmoothingFactor + (1 - fFFTSmoothingFactor) * val;
-        res += fftDataSmoothed[i];
-      }
-    } else {
-      res += val;
-    }
+    res += 2.0 * sqrtf(fftBuf[i].r * fftBuf[i].r + fftBuf[i].i * fftBuf[i].i) * scaling;
   }
 
   return res;
