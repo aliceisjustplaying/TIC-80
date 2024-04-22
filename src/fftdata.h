1dbc4acbe7230da6af427f3b4631244850257d4e
alice <aliceisjustplaying@gmail.com>
2024-04-22 20:04:42 +0100

    add FFT_DebugLog

--- src/fftdata.h
+++ src/fftdata.h
@@ -8,3 +8,5 @@ extern float fftData[FFT_SIZE];
 extern float fftSmoothingData[FFT_SIZE];
 extern float fftNormalizedData[FFT_SIZE];
 extern float fftNormalizedMaxData[FFT_SIZE];
+
+void FFT_DebugLog(const char* format, ...);
