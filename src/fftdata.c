d6d8be097a1c1e7d39d1ec387a7a8cf34d07388c
alice <aliceisjustplaying@gmail.com>
2024-04-22 20:04:42 +0100

    add FFT_DebugLog

--- src/fftdata.c
+++ src/fftdata.c
@@ -1,4 +1,18 @@
 #include "fftdata.h"
+#include <stdarg.h> // For va_list
+#include <stdio.h> // for vprintf
+
+#define FFT_DEBUG
+
+void FFT_DebugLog(const char* format, ...)
+{
+#ifdef FFT_DEBUG
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+#endif
+}
 float fPeakMinValue = 0.01f;
 float fPeakSmoothing = 0.995f;
 float fPeakSmoothValue = 0.0f;
