b0edb02771e37c79ce6ef80ecb83fa6e2e8e0ef6
alice <aliceisjustplaying@gmail.com>
2024-04-24 16:44:27 +0100

    update debug-macos.sh; current bug is not in glew

--- debug-macos.sh
+++ debug-macos.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 FRESH_FLAG=""
-if [ "$1" == "-f" ]; then
-    FRESH_FLAG="--fresh"
-fi
+CLEAN_FLAG=""
+DEBUG_FLAGS=""
+
+for arg in "$@"
+do
+    case $arg in
+        -f)
+            FRESH_FLAG="--fresh"
+            ;;
+        -c)
+            CLEAN_FLAG="--clean-first"
+            ;;
+        -d)
+            DEBUG_FLAGS="-DBUILD_NO_OPTIMIZATION=On -DBUILD_ASAN_DEBUG=On -DBUILD_LSAN_DEBUG=On -DBUILD_UNDEFINED_DEBUG=On"
+            ;;
+    esac
+done
 
 cd ./build || exit
-export PATH="/opt/homebrew/opt/llvm/bin:$PATH" && \
-export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++" && \
-export BUILD_TYPE=Debug && \
+
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+export BUILD_TYPE=Debug
+
 cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
       -DBUILD_PRO=On \
       -DBUILD_SDLGPU=On \
-      -DBUILD_NO_OPTIMIZATION=On \
-      -DBUILD_ASAN_DEBUG=On \
-      -DBUILD_LSAN_DEBUG=On \
-      -DBUILD_UNDEFINED_DEBUG=On \
+      "$DEBUG_FLAGS" \
       -DCMAKE_C_COMPILER="$(which clang)" \
       -DCMAKE_CXX_COMPILER="$(which clang++)" \
        .. $FRESH_FLAG && \
-       cmake --build . --config "$BUILD_TYPE" --parallel
+       cmake --build . --config "$BUILD_TYPE" --parallel $CLEAN_FLAG
