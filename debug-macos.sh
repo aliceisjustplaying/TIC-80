4897b13441235007539c10357f16a820d057b645
alice <aliceisjustplaying@gmail.com>
2024-04-24 18:09:21 +0100

    Add shell script to make debugging easier

---
+++ debug-macos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+FRESH_FLAG=""
+CLEAN_FLAG=""
+DEBUG_FLAGS=""
+
+for arg in "$@"
+do
+    case $arg in
+        -f)
+            FRESH_FLAG="--fresh"
+            ;;
+        -c)
+            CLEAN_FLAG="--clean-first"
+            ;;
+        -d)
+            DEBUG_FLAGS="-DBUILD_NO_OPTIMIZATION=On -DBUILD_ASAN_DEBUG=On -DBUILD_LSAN_DEBUG=On -DBUILD_UNDEFINED_DEBUG=On"
+            ;;
+    esac
+done
+
+if [[ "$CLEAN_FLAG" == "--clean-first" ]]; then
+    rm ./build/**/*.o
+    rm ./build/**/*.a
+    rm ./build/bin/tic80
+fi
+
+cd ./build || exit
+
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+export BUILD_TYPE=Debug
+
+cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+      -DBUILD_PRO=On \
+      -DBUILD_SDLGPU=On \
+      "$DEBUG_FLAGS" \
+      -DCMAKE_C_COMPILER="$(which clang)" \
+      -DCMAKE_CXX_COMPILER="$(which clang++)" \
+       .. $FRESH_FLAG && \
+       cmake --build . --config "$BUILD_TYPE" --parallel $CLEAN_FLAG
