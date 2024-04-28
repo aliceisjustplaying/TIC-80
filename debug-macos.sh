f755890cd506308f57ae7f8208bfb068261f7efb
alice <aliceisjustplaying@gmail.com>
2024-04-28 23:40:37 +0100

    debug-macos.sh

---
+++ debug-macos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+FRESH_FLAG=""
+DEBUG_FLAGS=""
+
+for arg in "$@"
+do
+    case $arg in
+        -f)
+            FRESH_FLAG="--fresh"
+            ;;
+        -d)
+            DEBUG_FLAGS="-DBUILD_NO_OPTIMIZATION=On -DBUILD_ASAN_DEBUG=On"
+            ;;
+    esac
+done
+
+if [ "$FRESH_FLAG" == "--fresh" ]; then
+    rm -rf .cache
+    rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake
+    rm -rf build && git restore 'build/*'
+fi
+
+cd ./build || exit
+
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++ -L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+export BUILD_TYPE=Debug
+
+cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+      -DBUILD_PRO=On \
+      -DBUILD_SDLGPU=On \
+      "$DEBUG_FLAGS" \
+      -DCMAKE_C_COMPILER="$(which clang)" \
+      -DCMAKE_CXX_COMPILER="$(which clang++)" \
+       .. $FRESH_FLAG && \
+       cmake --build . --config "$BUILD_TYPE" --parallel
