5509db8ca84d4eea168274ba785f7aa4b741ea3f
alice <aliceisjustplaying@gmail.com>
2024-04-28 23:15:06 +0100

    Initial copy of changes.

--- src/studio/screens/console.c
+++ src/studio/screens/console.c
@@ -4218,9 +4218,7 @@ static void tick(Console* console)
         }
         else printBack(console, "\n loading cart...");
     }
-
-    if (getStudioMode(console->studio) != TIC_CONSOLE_MODE) return;
-
+    
     tic_api_cls(tic, TIC_COLOR_BG);
     drawConsoleText(console);
 
@@ -4244,6 +4242,10 @@ static void tick(Console* console)
     {
         if(console->cursor.delay)
             console->cursor.delay--;
+
+        console->tickCounter++;
+
+        if (getStudioMode(console->studio) != TIC_CONSOLE_MODE) return;
 
         drawCursor(console);
 
